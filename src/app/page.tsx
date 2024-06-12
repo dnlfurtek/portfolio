41f16cd381c18fa9b82074cbbe7d92540241391d
Daniel Furtek <dnlfurtek@gmail.com>
2024-06-12 22:47:15 +0200

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,230 @@
-import Image from "next/image";
+"use client";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import XIcon from "@mui/icons-material/X";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import PersonIcon from "@mui/icons-material/Person";
+import WorkIcon from "@mui/icons-material/Work";
+import SchoolIcon from "@mui/icons-material/School";
+import MailIcon from "@mui/icons-material/Mail";
+import Pagination from "@mui/material/Pagination";
+import * as React from "react";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+import ContactMailIcon from "@mui/icons-material/ContactMail";
+import ContactPhoneIcon from "@mui/icons-material/ContactPhone";
+import Link from "@mui/material/Link";
 
+const Bio = () => (
+  <div className="articleContent">
+    <Typography variant="h3" className="articleHeader honk-font">
+      Bio
+    </Typography>
+    <Typography className="fira-code-font">
+      18 Years Old High School Student from Poland.
+    </Typography>
+  </div>
+);
+
+const Career = () => (
+  <div className="articleContent">
+    <Typography variant="h3" className="articleHeader honk-font">
+      Career
+    </Typography>
+    <Timeline position="alternate">
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary">
+          October 2022
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          Internship in Modern Forms, Dobrzechów
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary">
+          June 2024
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Resumed Internship in Modern Forms</TimelineContent>
+      </TimelineItem>
+    </Timeline>
+    <center>
+      <p color="text.secondary">
+        <i>To be continued...</i>
+      </p>
+    </center>
+  </div>
+);
+
+const Education = () => (
+  <div className="articleContent">
+    <Typography variant="h3" className="articleHeader honk-font">
+      Education
+    </Typography>
+    <Timeline position="alternate">
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary">
+          2012 / 2020
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          Elementary School in Szufnarowa, Poland
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary">
+          2020 / 2025
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          Technical and General Education School Complex in Strzyżów
+        </TimelineContent>
+      </TimelineItem>
+    </Timeline>
+    <center>
+      <p color="text.secondary">
+        <i>To be continued...</i>
+      </p>
+    </center>
+  </div>
+);
+
+const Contact = () => (
+  <div className="articleContent">
+    <Typography variant="h3" className="articleHeader honk-font">
+      Contact
+    </Typography>
+    <Typography className="fira-code-font" fontSize={15} mt={0}>
+      <div className="contact">
+        <ContactMailIcon />{" "}
+        <Link href="mailto: dnlfurtek@gmail.com" underline="hover">
+          dnlfurtek@gmail.com
+        </Link>
+      </div>
+    </Typography>
+    <Typography className="fira-code-font" fontSize={15} mt={0}>
+      <div className="contact">
+        <ContactPhoneIcon /> <p>(+48) 667-727-811</p>
+      </div>
+    </Typography>
+  </div>
+);
+
+// Main component
 export default function Home() {
+  const [currentPage, setPage] = React.useState(1);
+
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
+
+  // Determine which component to render based on currentPage
+  const renderContent = () => {
+    switch (currentPage) {
+      case 1:
+        return <Bio />;
+      case 2:
+        return <Career />;
+      case 3:
+        return <Education />;
+      case 4:
+        return <Contact />;
+      default:
+        return <Bio />;
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="container">
+      <div className="nav">
+        <Button variant="contained" className="socialButton">
+          <GitHubIcon className="socialIcon" />
+        </Button>
+        <Button variant="contained" className="socialButton">
+          <LinkedInIcon className="socialIcon" />
+        </Button>
+        <Button variant="contained" className="socialButton">
+          <XIcon className="socialIcon" />
+        </Button>
+        <Button variant="contained" className="socialButton">
+          <FacebookIcon className="socialIcon" />
+        </Button>
+      </div>
+      <div className="main">
+        <Avatar
+          className="avatar"
+          alt="Daniel Furtek"
+          src="https://i.pinimg.com/originals/2a/99/b1/2a99b169e671e83cee44c22b206b191d.png"
+        />
+        <div className="description">
+          <Typography>
+            <b>Daniel Furtek</b>
+            <Divider />
+            Aspiring Software Developer
+          </Typography>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="article">
+        {renderContent()}
+        <Pagination
+          className="pagination"
+          count={4}
+          page={currentPage}
+          onChange={handleChange}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="footer">
+        <Button
+          variant="contained"
+          className="footButton"
+          onClick={() => setPage(1)}
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          <PersonIcon className="footIcon" />
+        </Button>
+        <Button
+          variant="contained"
+          className="footButton"
+          onClick={() => setPage(2)}
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          <WorkIcon className="footIcon" />
+        </Button>
+        <Button
+          variant="contained"
+          className="footButton"
+          onClick={() => setPage(3)}
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          <SchoolIcon className="footIcon" />
+        </Button>
+        <Button
+          variant="contained"
+          className="footButton"
+          onClick={() => setPage(4)}
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <MailIcon className="footIcon" />
+        </Button>
       </div>
-    </main>
+    </div>
   );
 }
