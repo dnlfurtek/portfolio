0a9816276d8efb1a50abfa7c325f56d72162af61
dnlfurtek <95034536+dnlfurtek@users.noreply.github.com>
2024-06-12 20:57:21 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/portfolio",
-  output: "export",
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
+    output: "export"
+  };
+  
+  module.exports = nextConfig;
+  
